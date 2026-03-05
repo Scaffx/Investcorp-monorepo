afcf659963c16dba36e09e371da3d8614ff7ef2c
Murillo Scaff <126569253+Scaffx@users.noreply.github.com>
2026-03-05 17:26:58 -0300

    Update views.py

--- apps/api/api/views.py
+++ apps/api/api/views.py
@@ -1,7 +1,8 @@
 import os
+import tempfile
 import base64
-from django.conf import settings
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, parser_classes
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 
 # 1. Endpoint GET /api/health
